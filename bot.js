fc10785878461cddc3486563da44c6438cb26e0f
Bakdashinigami <31391450+Bakdashinigami@users.noreply.github.com>
2017-08-28 19:38:12 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+var bot = require("Noodle-Bot Music");
+ 
+var serverName = "Noodle Bot";
+var textChannelName = "Resturant";
+var voiceChannelName = "General";
+var botToken = "MzUxODg1MDU5NDg3MTcwNTYy.DIZGBQ.Bo2r77eMgg5mU0rEEmNaVXUWyEU";
+ 
+bot.run(serverName, textChannelName, voiceChannelName, botToken);
